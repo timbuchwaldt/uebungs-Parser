fc5cc359da36939c2cc11f777fbc58db1c51f574
Tim Buchwaldt <tim@21studios.de>
2011-05-26 14:06:35 +0200

    reworked everything for the sinatra server

--- server.rb
+++ server.rb
@@ -1,7 +1,14 @@
+# Includes
 require 'rubygems'
 require 'sinatra'
 require 'erb'
-require 'datamapper'
+require 'data_mapper'
+require 'models'
+
+# Setup
+DataMapper.setup(:default, "sqlite://#{Dir.pwd}/test.db")
+
+
 get '/' do
-  "Hello Tim"
+  erb :index, :locals => {:greeting => "tim"}
 end
